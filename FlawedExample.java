a815760340d5542a7b713fe3eed58c7444988976
theo-mandelbaum <mandelbaumtheo@gmail.com>
2024-10-08 15:22:34 -0400

    Good

--- FlawedExample.java
+++ FlawedExample.java
@@ -14,11 +14,16 @@ public class FlawedExample {
         System.out.println("Number of bits for N = " + hexNum);
 
         BigInteger p = new BigInteger("967148115841218361396434822121");
-        System.out.println("Number for P = " + p);
+        String p_hex = p.toString(16);
+        String bits_p = findBitCount(p);
+        System.out.println("P = " + p_hex);
+        System.out.println("Number of bits for P = " + bits_p);
 
         BigInteger q = new BigInteger("1220692183532008344492309072419");
-        String hexq = findBitCount(q);
-        System.out.println("Number of bits for Q = " + hexq);
+        String q_hex = q.toString(16);
+        String bits_q = findBitCount(q);
+        System.out.println("Q = " + q_hex);
+        System.out.println("Number of bits for Q = " + bits_q);
 
         BigInteger e = new BigInteger("65537");
         // z = (p - 1) * (q-1)
@@ -26,17 +31,19 @@ public class FlawedExample {
         BigInteger d = e.modInverse(z);
         String hex_d = d.toString(16);
         System.out.println("d = " + hex_d);
-        System.out.println("Number for Q = " + q);
         
         BigInteger m = new BigInteger("3");
         // Cypher Text = C
         // c = m^e mod N
         BigInteger c = m.modPow(e, number);
-        System.out.println("Cypher Text c = " + c);
+        String c_hex = c.toString(16);
+        System.out.println("Cypher Text c = " + c_hex);
 
         // decrypt by solving m' = c^d mod N
         BigInteger decryptedM = c.modPow(d, number);
-        System.out.println("Decrypted Message(3) = " + decryptedM);
+        String decryptedM_hex = decryptedM.toString(16);
+        // When the message (m = 3) is decrypted, the result is printed
+        System.out.println("Decrypted Message(3) = " + decryptedM_hex);
 
 
 
