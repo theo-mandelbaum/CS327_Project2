838a1c6f50ec6937a0b1af770f8176b5f27f439f
theo-mandelbaum <mandelbaumtheo@gmail.com>
2024-10-08 12:30:29 -0400

    BigInt

--- FlawedExample.java
+++ FlawedExample.java
@@ -1,3 +1,27 @@
+import java.math.BigInteger;
+
 public class FlawedExample {
-    
+
+    public int findBitCount (BigInteger num) {
+        int count = 0;
+        
+        return count;
+    }
+
+    public void testBitCount () {
+        BigInteger number = new BigInteger("1180590145325084590940239490622584768749437722435980772180699");
+
+
+
+    }
+    public static void main (String[] args) {
+		FlawedExample testObj = new FlawedExample ();
+
+		System.out.println ("********** Project 2 output begins ********** ");
+
+		//testObj.testGcd ();
+		//testObj.testXgcd ();
+	    //testObj.testKeygen ();
+		//testObj.testRSA ();
+	}
 }
